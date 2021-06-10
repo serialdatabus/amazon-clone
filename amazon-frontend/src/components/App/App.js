125f31bf533633f28815ae84db4e89afa7af8a84
Elizeu Carlos <elizeucarlos@mbp-de-elizeu.lan>
2021-06-10 16:44:44 +0100

    updates

---
+++ amazon-frontend/src/components/App/App.js
@@ -0,0 +1,17 @@
+import logo from '../../assets/logo.svg';
+import '../../styles/App.css';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+        
+        <Header></Header>
+
+    </div>
+  );
+}
+
+export default App;
+ 
+
