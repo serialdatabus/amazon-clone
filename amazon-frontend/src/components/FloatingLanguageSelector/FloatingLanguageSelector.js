a472146848ace756b82cabd46acd4bd8fea1f8b2
Elizeu Carlos <elizeucarlos@mbp-de-elizeu.lan>
2021-06-14 04:17:29 +0100

    changes

--- amazon-frontend/src/components/FloatingLanguageSelector/FloatingLanguageSelector.js
+++ amazon-frontend/src/components/FloatingLanguageSelector/FloatingLanguageSelector.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "../../styles/FloatingLanguageSelector.css";
-import { languages, renderLanguages } from "./controller";
+import { renderLanguages } from "./controller";
 
 export const FloatingLanguageSelector = ({ isvisible }) => {
   // const [isvisible, setisvisible] = useState(true);
