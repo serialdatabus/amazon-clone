ed7eb4ce0a279264c999182df22f77f28f5c6d0c
Elizeu Carlos <elizeucarlos@mbp-de-elizeu.lan>
2021-06-15 16:45:13 +0100

    changes

--- amazon-frontend/src/components/Header/NavBar.js
+++ amazon-frontend/src/components/Header/NavBar.js
@@ -1,29 +1,31 @@
-import { faBars, faCaretDown, faShoppingBasket } from "@fortawesome/free-solid-svg-icons"
-import { fonticon } from "../../helpers/helpers"
-
-
+import {
+  faBars,
+  faCaretDown,
+  faShoppingBasket,
+} from "@fortawesome/free-solid-svg-icons";
+import { fonticon } from "../../helpers/helpers";
 
 export const NavBar = ({ onOpenSideMenu }) => {
-    return (
-        <div id="header-navbar">
-        <div className="nav-left">
-          <a href="/" className="hamburger-menu" onClick={onOpenSideMenu}>
-            {fonticon(faBars, "iconbars")}
-            <span>All</span>
-          </a>
+  return (
+    <div id="header-navbar">
+      <div className="nav-left">
+        <a href="/" className="hamburger-menu" onClick={onOpenSideMenu}>
+          {fonticon(faBars, "iconbars")}
+          <span>All</span>
+        </a>
 
-          <a href="/">
-            <span>Browsing History</span>
-            {fonticon(faCaretDown, "icondown")}
-          </a>
-        </div>
-
-        <div className="nav-right">
-          <a href="/">
-            {fonticon(faShoppingBasket, "add-product-icon")}
-            <span>Add new product</span>
-          </a>
-        </div>
+        <a href="/">
+          <span>Browsing History</span>
+          {fonticon(faCaretDown, "icondown")}
+        </a>
       </div>
-    )
-}
+
+      <div className="nav-right">
+        <a href="/">
+          {fonticon(faShoppingBasket, "add-product-icon")}
+          <span>Add new product</span>
+        </a>
+      </div>
+    </div>
+  );
+};
