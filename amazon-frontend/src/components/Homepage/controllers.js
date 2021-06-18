18bcc5040a8b47e09f5b0749b0675044289f3594
Elizeu Carlos <elizeucarlos@mbp-de-elizeu.lan>
2021-06-18 06:03:29 +0100

    changes

--- amazon-frontend/src/components/Homepage/controllers.js
+++ amazon-frontend/src/components/Homepage/controllers.js
@@ -7,6 +7,11 @@ import { fonticon } from "../../helpers/helpers";
 export const imagesList = [
   {
     image_url:
+      "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg",
+    link: "https://youtube.com",
+  },
+  {
+    image_url:
       "https://images-eu.ssl-images-amazon.com/images/G/30/em/pd21/xcm_em_Prime_Day_2021_858-ESES_D_PDS-HP-Tall-Hero_1500x600._CB665999375_.jpg",
     link: "https://youtube.com",
   },
@@ -46,34 +51,28 @@ export const AmazonCarouselSettings = {
   prevArrow: <Arrow />,
   pauseOnHover: false,
   autoplay: true,
-  autoplaySpeed: 6000
+  autoplaySpeed: 6000,
 };
 
-
-
 export const blockhello = [
-    {
-      image_url:
-        "https://m.media-amazon.com/images/I/41qsnBY6yZL._AC_SY200_.jpg",
-      label: "Your Orders",
-      url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
-    },
-    {
-      image_url:
-        "https://m.media-amazon.com/images/I/31ZXXTy7wMS._AC_SY200_.jpg",
-      label: "Your Orders",
-      url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
-    },
-    {
-      image_url:
-        "https://m.media-amazon.com/images/I/51q0meZK6WL._AC_SY200_.jpg",
-      label: "Your Orders",
-      url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
-    },
-    {
-      image_url:
-        "https://m.media-amazon.com/images/I/41XMZLW8zbL._AC_SY200_.jpg",
-      label: "Your Orders",
-      url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
-    },
-  ];
+  {
+    image_url: "https://m.media-amazon.com/images/I/41qsnBY6yZL._AC_SY200_.jpg",
+    label: "Your Orders",
+    url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
+  },
+  {
+    image_url: "https://m.media-amazon.com/images/I/31ZXXTy7wMS._AC_SY200_.jpg",
+    label: "Your Orders",
+    url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
+  },
+  {
+    image_url: "https://m.media-amazon.com/images/I/51q0meZK6WL._AC_SY200_.jpg",
+    label: "Your Orders",
+    url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
+  },
+  {
+    image_url: "https://m.media-amazon.com/images/I/41XMZLW8zbL._AC_SY200_.jpg",
+    label: "Your Orders",
+    url: "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/?_encoding=UTF8&pd_rd_w=StUxV&pf_rd_p=9ac1d1b4-9983-4d1d-86fc-05f697271072&pf_rd_r=TB4B7NZ1YTT0SQBG4APS&pd_rd_r=7c6f913b-b32b-4dfc-aaf8-70df7d3e8252&pd_rd_wg=go6Cw&ref_=pd_gw_unk",
+  },
+];
